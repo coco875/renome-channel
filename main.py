896a65e81b40e698062ea1617691b64df6aef631
coco875 <59367621+coco875@users.noreply.github.com>
2022-11-20 02:33:19 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,193 @@
+import interactions
+from interactions import get
+import json
+from os import path
+
+with open("tokens.json") as f:
+    tokens = json.load(f)
+
+bot = interactions.Client(
+    token=tokens,
+    default_scope=1035865026285809674
+)
+
+if path.isfile("change.json"):
+    with open("change.json", "r", encoding="UTF-8") as f:
+        data:dict = json.load(f, parse_int=int, parse_float=float, )
+        print(data)
+else:
+    data = {}
+
+confirm_change = interactions.Button(
+    style=interactions.ButtonStyle.SUCCESS,
+    label="Confirmer",
+    custom_id="confirm change",
+    emoji=interactions.Emoji(name="✅")
+)
+
+anule_change = interactions.Button(
+    style=interactions.ButtonStyle.DANGER,
+    label="Annuler",
+    custom_id="anule change",
+    emoji=interactions.Emoji(name="❌")
+)
+
+# modal_confirm = interactions.Modal(
+#         title="Confirmation",
+#         custom_id="mod_app_form",
+#         components=[interactions.TextInput(
+#     style=interactions.TextStyleType.SHORT,
+#     label="Let's get straight to it: what's 1 + 1?",
+#     custom_id="text_input_response",
+#     min_length=1,
+#     max_length=3,
+# )]
+# )
+
+@bot.command(
+    name="change",
+    description="change le nom de channel",
+    options = [
+        interactions.Option(
+            name="voice_channel",
+            description="Le channel vocal",
+            type=interactions.OptionType.CHANNEL,
+            required=True,
+        ),
+    ],
+)
+async def change(ctx: interactions.CommandContext, voice_channel: interactions.Channel):    
+    if voice_channel.type != interactions.ChannelType.GUILD_VOICE:
+        await ctx.send("Ce channel n'est pas un channel vocal")
+        return
+
+    if not str(voice_channel.id) in data:
+        await ctx.send("Ce channel n'est pas enregistré, vous pouvez l'enregistrer avec la commande `register`")
+        return
+    
+    if not voice_channel.name in data[str(voice_channel.id)]:
+        await ctx.send("Ce channel n'as pas un nom valide, vous pouvez le rénitiliser avec la commande `reset`")
+        return
+    
+    l:list = data[str(voice_channel.id)]
+
+    
+    await ctx.send("Quel nom voulez vous donner au channel ?", components=interactions.SelectMenu(
+        custom_id="select_menu",
+        options=[
+            interactions.SelectOption(
+                label=i,
+                value=str(voice_channel.id)+" "+str(l.index(i)),
+        ) for i in l]
+        )
+    )
+
+@bot.component("select_menu")
+async def select_menu(ctx: interactions.ComponentContext, choice):
+    answer = choice[0].split(" ")
+    l = data[answer[0]]
+    await ctx.send("Voulez vous vraiment changer le nom du channel par " + l[int(answer[1])] + " ?", components=[confirm_change, anule_change])
+
+@bot.component("confirm change")
+async def confirm_changed(ctx: interactions.ComponentContext):
+    channel = await ctx.get_channel()
+    last_message = await channel.get_history(limit=2)
+    name = last_message[0].content.replace("Voulez vous vraiment changer le nom du channel par ", "").replace(" ?", "")
+    id_channel = last_message[1].components[0].components[0].options[0].value.split(" ")[0]
+    print(id_channel)
+    channel = await get(bot, interactions.Channel, object_id=id_channel)
+    await channel.modify(name=name)
+    await ctx.edit("Le nom du channel a été changé avec succès", components=[])
+
+
+
+@bot.command(
+    name="register",
+    description="enregistre un channel vocal",
+    options = [
+        interactions.Option(
+            name="voice_channel",
+            description="le channel vocal à enregistrer",
+            type=interactions.OptionType.CHANNEL,
+            required=True,
+        ),
+        interactions.Option(
+            name="names",
+            description="les noms à utiliser",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ],
+)
+async def register(ctx: interactions.CommandContext, voice_channel: interactions.Channel, names: str):
+    if voice_channel.type != interactions.ChannelType.GUILD_VOICE:
+        await ctx.send("Ce channel n'est pas un channel vocal")
+        return
+    
+    if str(voice_channel.id) in data:
+        await ctx.send("Ce channel est déjà enregistré")
+        return
+    
+    data[str(voice_channel.id)] = names.split(" ")
+    with open("change.json", "w", encoding="UTF-8") as f:
+        json.dump(data, f, indent=4)
+    
+    await ctx.send("Le channel a été enregistré")
+
+@bot.command(
+    name="reset",
+    description="réinitialise le nom d'un channel vocal",
+    options = [
+        interactions.Option(
+            name="voice_channel",
+            description="le channel vocal à réinitialiser",
+            type=interactions.OptionType.CHANNEL,
+            required=True,
+        ),
+    ],
+)
+async def reset(ctx: interactions.CommandContext, voice_channel: interactions.Channel):
+    if voice_channel.type != interactions.ChannelType.GUILD_VOICE:
+        await ctx.send("Ce channel n'est pas un channel vocal")
+        return
+    
+    if not str(voice_channel.id) in data:
+        await ctx.send("Ce channel n'est pas enregistré, vous pouvez l'enregistrer avec la commande `register`")
+        return
+    
+    await voice_channel.modify(name=data[str(voice_channel.id)][0])
+    await ctx.send("Le nom a été réinitialisé")
+
+@bot.command(
+    name="unregister",
+    description="désenregistre un channel vocal",
+    options = [
+        interactions.Option(
+            name="voice_channel",
+            description="le channel vocal à désenregistrer",
+            type=interactions.OptionType.CHANNEL,
+            required=True,
+        ),
+    ],
+)
+async def unregister(ctx: interactions.CommandContext, voice_channel: interactions.Channel):
+    if voice_channel.type != interactions.ChannelType.GUILD_VOICE:
+        await ctx.send("Ce channel n'est pas un channel vocal")
+        return
+    
+    if not str(voice_channel.id) in data:
+        await ctx.send("Ce channel n'est pas enregistré, vous pouvez l'enregistrer avec la commande `register`")
+        return
+    
+    del data[str(voice_channel.id)]
+    with open("change.json", "w", encoding="UTF-8") as f:
+        json.dump(data, f, indent=4)
+    
+    await ctx.send("Le channel a été désenregistré")
+
+
+@bot.event
+async def on_ready():
+    print("Ready!")
+
+bot.start()
